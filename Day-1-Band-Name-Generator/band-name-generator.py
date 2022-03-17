579fc5589106aa6bbb7a770e7526089e4ef8ec2d
KaziTanvir <kazitanvir.bangladesh@gmail.com>
2022-03-17 22:51:38 +0600

    Day-1-Band-Name-Generator

---
+++ Day-1-Band-Name-Generator/band-name-generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator: ")
+
+a= input("Enter the name of the city that you are born: ")
+b= input("Enter the name of your pet: ")
+
+print(f"The suggested name of the band is: {a} {b}")
---
+++ Day-1-Band-Name-Generator/main.py
@@ -0,0 +1,11 @@
+a = input("a: ")
+b = input("b: ")
+temp1 = 0;
+temp2 = 0;
+temp1 = a
+temp2 = b
+b = temp1
+a = temp2
+
+print(f"A: {a}")
+print(f"B: {b}")
